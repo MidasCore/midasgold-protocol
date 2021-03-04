0f1fe7ccb3fdec0ee8dd046e7dfbbfa6ddf8d308
snguyen.ntu@gmail.com <hellobaby0202>
2021-03-04 16:40:07 +0700

    Updated Pool contract. Added testcase

--- contracts/MdgRewardPool.sol
+++ contracts/MdgRewardPool.sol
@@ -29,17 +29,22 @@ contract MdgRewardPool {
     // governance
     address public operator = address(0xD025628eEe504330f1282C96B28a731E3995ff66);
 
+    // flags
+    bool public initialized = false;
+
     address public reserveFund;
     uint256 public reservePercent = 1000; // 100 = 1%
     uint256 public burnPercent = 250;
 
     uint256 public lockPercent = 7500; // 75%
-    IMdgLocker mdgLocker = IMdgLocker(0x0000000000000000000000000000000000000000);
+    IMdgLocker mdgLocker;
 
     // Info of each user.
     struct UserInfo {
         uint256 amount; // How many LP tokens the user has provided.
         uint256 rewardDebt; // Reward debt. See explanation below.
+        uint256 mdoDebt;
+        uint256 bcashDebt;
     }
 
     // Info of each pool.
@@ -48,11 +53,15 @@ contract MdgRewardPool {
         uint256 allocPoint; // How many allocation points assigned to this pool. MDGs to distribute per block.
         uint256 lastRewardBlock; // Last block number that MDGs distribution occurs.
         uint256 accMdgPerShare; // Accumulated MDGs per share, times 1e18. See below.
+        uint256 accMdoPerShare;
+        uint256 accBcashPerShare;
         uint16 depositFeeBP; // Deposit fee in basis points
         bool isStarted; // if lastRewardBlock has passed
     }
 
     address public mdg = address(0xC1eDCc306E6faab9dA629efCa48670BE4678779D);
+    address public mdo = address(0x35e869B7456462b81cdB5e6e42434bD27f3F788c);
+    address public bcash = address(0xc2161d47011C4065648ab9cDFd0071094228fa09);
 
     // Info of each pool.
     PoolInfo[] public poolInfo;
@@ -64,6 +73,8 @@ contract MdgRewardPool {
     uint256 public totalAllocPoint = 0;
 
     uint256 public rewardPerBlock;
+    uint256 public mdoPerBlock;
+    uint256 public bcashPerBlock;
 
     // The block number when mdg mining starts.
     uint256 public startBlock;
@@ -75,27 +86,49 @@ contract MdgRewardPool {
     // The liquidity migrator contract. It has a lot of power. Can only be set through governance (owner).
     ILiquidityMigrator public migrator;
 
+    event Initialized(address indexed executor, uint256 at);
     event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
     event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
     event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
-    event RewardPaid(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, address indexed token, uint256 amount);
 
-    constructor(
+    function initialize(
         address _mdg,
+        address _mdo,
+        address _bcash,
         uint256 _rewardPerBlock,
         uint256 _startBlock,
         uint256 _lockUntilBlock,
         address _mdgLocker,
+        address _reserveFund,
         address _operator
-    ) public {
+    ) public notInitialized {
         require(block.number < _startBlock, "late");
-        if (_mdg != address(0)) mdg = _mdg;
+        mdg = _mdg;
+        mdo = _mdo;
+        bcash = _bcash;
         rewardPerBlock = _rewardPerBlock; // start at 0.2 MDG per block for the first week
         startBlock = _startBlock; // supposed to be 5,383,000 (Thu Mar 04 2021 17:00:00 GMT+8)
         lockUntilBlock = _lockUntilBlock;
         nextHalvingBlock = startBlock.add(BLOCKS_PER_WEEK);
-        if (_mdgLocker != address(0)) mdgLocker = IMdgLocker(_mdgLocker);
+        mdgLocker = IMdgLocker(_mdgLocker);
+        reserveFund = _reserveFund;
         operator = _operator;
+
+        reservePercent = 1000; // 10%
+        burnPercent = 250; // 2.5%
+        lockPercent = 7500; // 75%
+
+        mdoPerBlock = 0.01 ether;
+        bcashPerBlock = 0.01 ether;
+
+        initialized = true;
+        emit Initialized(msg.sender, block.number);
+    }
+
+    modifier notInitialized() {
+        require(!initialized, "initialized");
+        _;
     }
 
     modifier onlyOperator() {
@@ -149,6 +182,8 @@ contract MdgRewardPool {
         uint256 _lastRewardBlock
     ) public onlyOperator {
         require(_depositFeeBP <= 10000, "add: invalid deposit fee basis points");
+        require(_allocPoint <= 500000, "too high allocation point"); // <= 500x
+        require(_depositFeeBP <= 1000, "too high fee"); // <= 10%
         checkPoolDuplicate(_lpToken);
         massUpdatePools();
         if (block.number < startBlock) {
@@ -174,6 +209,8 @@ contract MdgRewardPool {
             allocPoint : _allocPoint,
             lastRewardBlock : _lastRewardBlock,
             accMdgPerShare : 0,
+            accMdoPerShare : 0,
+            accBcashPerShare : 0,
             depositFeeBP : _depositFeeBP,
             isStarted : _isStarted
             }));
@@ -185,6 +222,8 @@ contract MdgRewardPool {
     // Update the given pool's mdg allocation point. Can only be called by the owner.
     function set(uint256 _pid, uint256 _allocPoint, uint16 _depositFeeBP) public onlyOperator {
         require(_depositFeeBP <= 10000, "set: invalid deposit fee basis points");
+        require(_allocPoint <= 500000, "too high allocation point"); // <= 500x
+        require(_depositFeeBP <= 1000, "too high fee"); // <= 10%
         massUpdatePools();
         PoolInfo storage pool = poolInfo[_pid];
         if (pool.isStarted) {
@@ -196,16 +235,16 @@ contract MdgRewardPool {
         pool.depositFeeBP = _depositFeeBP;
     }
 
-    // Return accumulate rewards over the given _from to _to block.
-    function getGeneratedReward(uint256 _from, uint256 _to) public view returns (uint256) {
+    // Return accumulate rewarded blocks over the given _from to _to block.
+    function getRewardBlocks(uint256 _from, uint256 _to) public view returns (uint256) {
         if (_from >= _to) return 0;
         if (_to <= startBlock) {
             return 0;
         } else {
             if (_from <= startBlock) {
-                return rewardPerBlock.mul(_to.sub(startBlock));
+                return _to.sub(startBlock);
             } else {
-                return rewardPerBlock.mul(_to.sub(_from));
+                return _to.sub(_from);
             }
         }
     }
@@ -217,11 +256,37 @@ contract MdgRewardPool {
         uint256 accMdgPerShare = pool.accMdgPerShare;
         uint256 lpSupply = pool.lpToken.balanceOf(address(this));
         if (block.number > pool.lastRewardBlock && lpSupply != 0) {
-            uint256 _generatedReward = getGeneratedReward(pool.lastRewardBlock, block.number);
+            uint256 _generatedReward = getRewardBlocks(pool.lastRewardBlock, block.number).mul(rewardPerBlock);
             uint256 _mdgReward = _generatedReward.mul(pool.allocPoint).div(totalAllocPoint);
             accMdgPerShare = accMdgPerShare.add(_mdgReward.mul(1e18).div(lpSupply));
         }
         return user.amount.mul(accMdgPerShare).div(1e18).sub(user.rewardDebt);
+    }
+
+    function pendingMdo(uint256 _pid, address _user) external view returns (uint256) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accMdoPerShare = pool.accMdoPerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+            uint256 _generatedReward = getRewardBlocks(pool.lastRewardBlock, block.number).mul(mdoPerBlock);
+            uint256 _mdoReward = _generatedReward.mul(pool.allocPoint).div(totalAllocPoint);
+            accMdoPerShare = accMdoPerShare.add(_mdoReward.mul(1e18).div(lpSupply));
+        }
+        return user.amount.mul(accMdoPerShare).div(1e18).sub(user.mdoDebt);
+    }
+
+    function pendingBcash(uint256 _pid, address _user) external view returns (uint256) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accBcashPerShare = pool.accBcashPerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+            uint256 _generatedReward = getRewardBlocks(pool.lastRewardBlock, block.number).mul(bcashPerBlock);
+            uint256 _bcashReward = _generatedReward.mul(pool.allocPoint).div(totalAllocPoint);
+            accBcashPerShare = accBcashPerShare.add(_bcashReward.mul(1e18).div(lpSupply));
+        }
+        return user.amount.mul(accBcashPerShare).div(1e18).sub(user.bcashDebt);
     }
 
     // Update reward variables for all pools. Be careful of gas spending!
@@ -248,11 +313,35 @@ contract MdgRewardPool {
             totalAllocPoint = totalAllocPoint.add(pool.allocPoint);
         }
         if (totalAllocPoint > 0) {
-            uint256 _generatedReward = getGeneratedReward(pool.lastRewardBlock, block.number);
-            uint256 _mdgReward = _generatedReward.mul(pool.allocPoint).div(totalAllocPoint);
+            uint256 _generatedReward = getRewardBlocks(pool.lastRewardBlock, block.number);
+            uint256 _mdgReward = _generatedReward.mul(rewardPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            uint256 _mdoReward = _generatedReward.mul(mdoPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            uint256 _bcashReward = _generatedReward.mul(bcashPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
             pool.accMdgPerShare = pool.accMdgPerShare.add(_mdgReward.mul(1e18).div(lpSupply));
+            pool.accMdoPerShare = pool.accMdoPerShare.add(_mdoReward.mul(1e18).div(lpSupply));
+            pool.accBcashPerShare = pool.accBcashPerShare.add(_bcashReward.mul(1e18).div(lpSupply));
         }
         pool.lastRewardBlock = block.number;
+    }
+
+    function _harvestReward(uint256 _pid, address _account) internal {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_account];
+        uint256 _pendingReward = user.amount.mul(pool.accMdgPerShare).div(1e18).sub(user.rewardDebt);
+        if (_pendingReward > 0) {
+            _safeMdgMint(_account, _pendingReward);
+            emit RewardPaid(_account, mdg, _pendingReward);
+        }
+        uint256 _pendingMdo = user.amount.mul(pool.accMdoPerShare).div(1e18).sub(user.mdoDebt);
+        if (_pendingMdo > 0) {
+            _safeTokenTransfer(mdo, _account, _pendingMdo);
+            emit RewardPaid(_account, mdo, _pendingMdo);
+        }
+        uint256 _pendingBcash = user.amount.mul(pool.accBcashPerShare).div(1e18).sub(user.bcashDebt);
+        if (_pendingBcash > 0) {
+            _safeTokenTransfer(bcash, _account, _pendingBcash);
+            emit RewardPaid(_account, bcash, _pendingBcash);
+        }
     }
 
     // Deposit LP tokens.
@@ -262,11 +351,7 @@ contract MdgRewardPool {
         UserInfo storage user = userInfo[_pid][_sender];
         updatePool(_pid);
         if (user.amount > 0) {
-            uint256 _pending = user.amount.mul(pool.accMdgPerShare).div(1e18).sub(user.rewardDebt);
-            if (_pending > 0) {
-                safeMdgMint(_sender, _pending);
-                emit RewardPaid(_sender, _pending);
-            }
+            _harvestReward(_pid, _sender);
         }
         if (_amount > 0) {
             pool.lpToken.safeTransferFrom(_sender, address(this), _amount);
@@ -279,6 +364,8 @@ contract MdgRewardPool {
             }
         }
         user.rewardDebt = user.amount.mul(pool.accMdgPerShare).div(1e18);
+        user.mdoDebt = user.amount.mul(pool.accMdoPerShare).div(1e18);
+        user.bcashDebt = user.amount.mul(pool.accBcashPerShare).div(1e18);
         emit Deposit(_sender, _pid, _amount);
     }
 
@@ -289,16 +376,14 @@ contract MdgRewardPool {
         UserInfo storage user = userInfo[_pid][_sender];
         require(user.amount >= _amount, "withdraw: not good");
         updatePool(_pid);
-        uint256 _pending = user.amount.mul(pool.accMdgPerShare).div(1e18).sub(user.rewardDebt);
-        if (_pending > 0) {
-            safeMdgMint(_sender, _pending);
-            emit RewardPaid(_sender, _pending);
-        }
+        _harvestReward(_pid, _sender);
         if (_amount > 0) {
             user.amount = user.amount.sub(_amount);
             pool.lpToken.safeTransfer(_sender, _amount);
         }
         user.rewardDebt = user.amount.mul(pool.accMdgPerShare).div(1e18);
+        user.mdoDebt = user.amount.mul(pool.accMdoPerShare).div(1e18);
+        user.bcashDebt = user.amount.mul(pool.accBcashPerShare).div(1e18);
         emit Withdraw(_sender, _pid, _amount);
     }
 
@@ -309,11 +394,13 @@ contract MdgRewardPool {
         uint256 _amount = user.amount;
         user.amount = 0;
         user.rewardDebt = 0;
+        user.mdoDebt = 0;
+        user.bcashDebt = 0;
         pool.lpToken.safeTransfer(msg.sender, _amount);
         emit EmergencyWithdraw(msg.sender, _pid, _amount);
     }
 
-    function safeMdgMint(address _to, uint256 _amount) internal {
+    function _safeMdgMint(address _to, uint256 _amount) internal {
         if (MidasGoldToken(mdg).minters(address(this)) && _to != address(0)) {
             uint256 _totalSupply = MidasGoldToken(mdg).totalSupply();
             uint256 _cap = MidasGoldToken(mdg).cap();
@@ -348,14 +435,41 @@ contract MdgRewardPool {
         }
     }
 
+    function _safeTokenTransfer(address _token, address _to, uint256 _amount) internal {
+        uint256 _tokenBal = IERC20(_token).balanceOf(address(this));
+        if (_amount > _tokenBal) {
+            _amount = _tokenBal;
+        }
+        if (_amount > 0) {
+            IERC20(_token).safeTransfer(_to, _amount);
+        }
+    }
+
     function setOperator(address _operator) external onlyOperator {
         operator = _operator;
+    }
+
+    function setRewardPerBlock(uint256 _rewardPerBlock) external onlyOperator {
+        require(_rewardPerBlock <= 0.2 ether, "too high reward"); // <= 0.2 MDG per block
+        _rewardPerBlock = _rewardPerBlock;
+    }
+
+    function setMdoPerBlock(uint256 _mdoPerBlock) external onlyOperator {
+        require(_mdoPerBlock <= 0.2 ether, "too high reward"); // <= 0.2 MDO per block
+        mdoPerBlock = _mdoPerBlock;
+    }
+
+    function setBcashPerBlock(uint256 _bcashPerBlock) external onlyOperator {
+        require(_bcashPerBlock <= 0.2 ether, "too high reward"); // <= 0.2 bCash per block
+        bcashPerBlock = _bcashPerBlock;
     }
 
     function governanceRecoverUnsupported(IERC20 _token, uint256 amount, address to) external onlyOperator {
         if (block.number < startBlock + BLOCKS_PER_WEEK * 104) {
             // do not allow to drain lpToken if less than 2 years after farming
-            require(address(_token) != mdg, "!mdg");
+            require(address(_token) != mdg, "mdg");
+            require(address(_token) != mdo, "mdo");
+            require(address(_token) != bcash, "bcash");
             uint256 length = poolInfo.length;
             for (uint256 pid = 0; pid < length; ++pid) {
                 PoolInfo storage pool = poolInfo[pid];
---
+++ test/MdgRewardPool.test.ts
@@ -0,0 +1,169 @@
+import chai, {expect} from 'chai';
+import {ethers} from 'hardhat';
+import {solidity} from 'ethereum-waffle';
+import {Contract, ContractFactory, BigNumber, utils} from 'ethers';
+import {Provider} from '@ethersproject/providers';
+import {SignerWithAddress} from 'hardhat-deploy-ethers/dist/src/signer-with-address';
+
+import {
+    ADDRESS_ZERO,
+    advanceBlock,
+    advanceTimeAndBlock, fromWei,
+    getLatestBlockNumber,
+    maxUint256,
+    mineBlocks,
+    toWei
+} from './shared/utilities';
+
+chai.use(solidity);
+
+async function latestBlocktime(provider: Provider): Promise<number> {
+    const {timestamp} = await provider.getBlock('latest');
+    return timestamp;
+}
+
+async function latestBlocknumber(provider: Provider): Promise<number> {
+    return await provider.getBlockNumber();
+}
+
+describe('MdgRewardPool.test', () => {
+    const {provider} = ethers;
+
+    let operator: SignerWithAddress;
+    let reserveFund: SignerWithAddress;
+    let bob: SignerWithAddress;
+    let carol: SignerWithAddress;
+    let david: SignerWithAddress;
+
+    before('provider & accounts setting', async () => {
+        [operator, reserveFund, bob, carol, david] = await ethers.getSigners();
+    });
+
+    // core
+    let MdgRewardPool: ContractFactory;
+    let MdgLocker: ContractFactory;
+    let MidasGoldToken: ContractFactory;
+    let MockERC20: ContractFactory;
+
+    before('fetch contract factories', async () => {
+        MdgRewardPool = await ethers.getContractFactory('MdgRewardPool');
+        MdgLocker = await ethers.getContractFactory('MdgLocker');
+        MidasGoldToken = await ethers.getContractFactory('MidasGoldToken');
+        MockERC20 = await ethers.getContractFactory('MockERC20');
+    });
+
+    let pool: Contract;
+    let locker: Contract;
+    let mdg: Contract;
+    let mdo: Contract;
+    let bcash: Contract;
+    let lpMain: Contract;
+    let lpFee: Contract;
+
+    let startBlock: BigNumber;
+    let rewardPerBlock: BigNumber;
+
+    before('deploy contracts', async () => {
+        mdg = await MidasGoldToken.connect(operator).deploy(toWei('1000000'));
+        mdo = await MockERC20.connect(operator).deploy('Midas Dollar', 'MDO', 18);
+        bcash = await MockERC20.connect(operator).deploy('bCash', 'bCash', 18);
+        lpMain = await MockERC20.connect(operator).deploy('lpMain', 'lpMain', 18);
+        lpFee = await MockERC20.connect(operator).deploy('lpFee', 'lpFee', 18);
+
+        startBlock = BigNumber.from(await latestBlocknumber(provider)).add(10);
+        locker = await MdgLocker.connect(operator).deploy(mdg.address, startBlock.add(10), startBlock.add(110));
+
+        pool = await MdgRewardPool.connect(operator).deploy();
+        rewardPerBlock = toWei('0.2');
+        await pool.connect(operator).initialize(mdg.address, mdo.address, bcash.address, rewardPerBlock, startBlock, startBlock.add(10), locker.address, reserveFund.address, operator.address);
+
+        await pool.connect(operator).add(20000, lpMain.address, 0, startBlock.add(20));
+        await pool.connect(operator).add(6000, lpFee.address, 400, 0);
+
+        await mdg.connect(operator).addMinter(pool.address);
+        await mdg.connect(operator).mint(bob.address, toWei('1000'));
+        await mdg.connect(bob).approve(pool.address, maxUint256);
+
+        await lpMain.mint(bob.address, toWei('1000'));
+        await lpMain.connect(bob).approve(pool.address, maxUint256);
+
+        await lpFee.mint(bob.address, toWei('1000'));
+        await lpFee.connect(bob).approve(pool.address, maxUint256);
+
+        await mdo.mint(pool.address, toWei('1000'));
+        await bcash.mint(pool.address, toWei('1000'));
+    });
+
+    describe('#initialize', () => {
+        it('should works correctly', async () => {
+            expect(String(await pool.mdg())).to.eq(mdg.address);
+            expect(String(await pool.startBlock())).to.eq('15');
+            expect(String(await pool.lockUntilBlock())).to.eq('25');
+            expect(String(await pool.rewardPerBlock())).to.eq(toWei('0.2'));
+            expect(String(await pool.mdoPerBlock())).to.eq(toWei('0.01'));
+            expect(String(await pool.bcashPerBlock())).to.eq(toWei('0.01'));
+            expect(String(await pool.totalAllocPoint())).to.eq('6000'); // 20x pool has not started
+        });
+    });
+
+    describe('#deposit', () => {
+        it('bob deposit 10 lpMain (no fee)', async () => {
+            await expect(async () => {
+                await pool.connect(bob).deposit(0, toWei('10'));
+            }).to.changeTokenBalances(lpMain, [bob, pool, reserveFund], [toWei('-10'), toWei('10'), toWei('0')]);
+            const _userInfo = await pool.userInfo(0, bob.address);
+            // console.log(_userInfo);
+            expect(_userInfo.amount).to.eq(toWei('10'));
+        });
+
+        it('bob deposit 10 lpFee (4% fee)', async () => {
+            await expect(async () => {
+                await pool.connect(bob).deposit(1, toWei('10'));
+            }).to.changeTokenBalances(lpFee, [bob, pool, reserveFund], [toWei('-10'), toWei('9.6'), toWei('0.4')]);
+            const _userInfo = await pool.userInfo(1, bob.address);
+            // console.log(_userInfo);
+            expect(_userInfo.amount).to.eq(toWei('9.6'));
+        });
+
+        it('bob withdraw some lpMain', async () => {
+            expect(String(await pool.pendingReward(0, bob.address))).to.eq(toWei('0'));
+            expect(String(await pool.pendingMdo(0, bob.address))).to.eq(toWei('0'));
+            expect(String(await pool.pendingBcash(0, bob.address))).to.eq(toWei('0'));
+            await mineBlocks(ethers, 20);
+            await pool.connect(bob).deposit(0, toWei('10'));
+            expect(String(await pool.totalAllocPoint())).to.eq('26000'); // 20x pool has started
+            const currentBlk = await getLatestBlockNumber(ethers);
+            console.log('currentBlk = %s', currentBlk);
+            await mineBlocks(ethers, 1);
+            expect(String(await pool.pendingReward(0, bob.address))).to.eq(toWei('0.15384615384615384'));
+            expect(String(await pool.pendingMdo(0, bob.address))).to.eq(toWei('0.00769230769230768'));
+            expect(String(await pool.pendingBcash(0, bob.address))).to.eq(toWei('0.00769230769230768'));
+            // console.log('burnPercent = %s', String (await pool.burnPercent()));
+            // await pool.connect(bob).withdraw(1, toWei('0'));
+            // await expect(pool.connect(bob).withdraw(1, toWei('0'))).to.emit(mdg, "Transfer").withArgs(pool.address, ADDRESS_ZERO, toWei('0.026538461538461535'));
+            // console.log('debug_burnAmount = %s', String (await pool.debug_burnAmount()));
+            let _beforeMdg = await mdg.balanceOf(bob.address);
+            let _beforeReserve = await mdg.balanceOf(reserveFund.address);
+            await expect(async () => {
+                await pool.connect(bob).withdraw(0, toWei('0'));
+            }).to.changeTokenBalances(mdo, [bob, pool], [toWei('0.01538461538461538'), toWei('-0.01538461538461538')]);
+            let _afterMdg = await mdg.balanceOf(bob.address);
+            let _afterReserve = await mdg.balanceOf(reserveFund.address);
+            expect(_afterMdg.sub(_beforeMdg)).to.eq(toWei('0.307692307692307680'));
+            expect(_afterReserve.sub(_beforeReserve)).to.eq(toWei('0.030769230769230768'));
+        });
+
+        it('bob withdraw all lpFee', async () => {
+            await expect(pool.connect(bob).withdraw(1, toWei('9.61'))).to.revertedWith('withdraw: not good');
+            let _beforeMdg = await mdg.balanceOf(bob.address);
+            let _beforeReserve = await mdg.balanceOf(reserveFund.address);
+            await expect(async () => {
+                await pool.connect(bob).withdraw(1, toWei('9.6'));
+            }).to.changeTokenBalances(mdo, [bob, pool], [toWei('0.057692307692307686'), toWei('-0.057692307692307686')]);
+            let _afterMdg = await mdg.balanceOf(bob.address);
+            let _afterReserve = await mdg.balanceOf(reserveFund.address);
+            expect(_afterMdg.sub(_beforeMdg)).to.eq(toWei('1.153846153846153843'));
+            expect(_afterReserve.sub(_beforeReserve)).to.eq(toWei('0.1153846153846153843'));
+        });
+    });
+});
